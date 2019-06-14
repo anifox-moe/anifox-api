11f2fa4fc371142f6214a37df35dee101bf51de7
Nathanial Howard <nathanial55@gmail.com>
2019-06-14 17:56:50 +0100

    Added reject to promise on db hook

--- src/db.js
+++ src/db.js
@@ -1,11 +1,15 @@
 import mysql from 'promise-mysql'
 
-export default new Promise(async resolve => {
-  const db = await mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME
-  })
-  resolve(db)
+export default new Promise(async (resolve, reject) => {
+  try {
+    const db = await mysql.createConnection({
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME
+    })
+    resolve(db)
+  } catch(e) {
+    reject(e)
+  }
 })
