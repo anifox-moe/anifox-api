277a09dc38f57f8023c34f186b3ebc6a2d2f23b2
Nathanial Howard <nathanial55@gmail.com>
2019-06-28 15:38:00 +0100

    Initialise tables on startup

--- src/db.js
+++ src/db.js
@@ -6,10 +6,48 @@ export default new Promise(async (resolve, reject) => {
       host: process.env.DB_HOST,
       user: process.env.DB_USER,
       password: process.env.DB_PASS,
-      database: process.env.DB_NAME
+      database: 'blah'
     })
+    initialiseTables(db) // Create tables in database
     resolve(db)
   } catch (e) {
     reject(e)
   }
 })
+
+const initialiseTables = async (db) => {
+  try {
+    // Anime table
+    await db.query(`
+    CREATE TABLE if not exists anime (
+      malID INT PRIMARY KEY NOT NULL,
+      type VARCHAR(10),
+      picture TEXT,
+      synopsis TEXT,
+      licensor VARCHAR(100),
+      title VARCHAR(255),
+      link TEXT,
+      genres TEXT,
+      producers VARCHAR(100),
+      fromType VARCHAR(50),
+      nbEp INT,
+      releaseDate int
+    );`)
+    // Episodes table
+    await db.query(`
+    CREATE TABLE if not exists episodes (
+      malID int NOT NULL,
+      FOREIGN KEY (malID) REFERENCES anime(malID),
+      epNumber VARCHAR(20),
+      PRIMARY KEY (malID, epNumber),
+      category VARCHAR(10),
+      resolution VARCHAR(20),
+      aired INT,
+      link TEXT,
+      torrent TEXT,
+      magnet TEXT
+    );`)
+  } catch (e) {
+    console.log(e)
+  }
+}
